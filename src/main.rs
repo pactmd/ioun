de893951bf187540ae59d270cade800a4a37b836
lucasfelber <lucas.felber@inode.at>
2024-09-25 18:32:55 +0200

    Fixed docker binding

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod routes;
 
 #[tokio::main]
 async fn main() {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
 
