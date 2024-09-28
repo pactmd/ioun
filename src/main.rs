9b01bab130bdb6907ac3f05aa2876f4b31207ea2
lucasfelber <lucas.felber@inode.at>
2024-09-28 23:10:43 +0200

    added tracing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use axum::Router;
+use axum::{extract::MatchedPath, http::Request, Router};
+use tower_http::trace::TraceLayer;
+use tracing::info_span;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod database;
 mod routes;
@@ -6,6 +9,22 @@ mod routes;
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+
+    tracing::debug!("before tracing registry");
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!(
+                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    env!("CARGO_CRATE_NAME")
+                ).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::debug!("after tracing registry");
 
     let postgres_pool = database::postgres::connect()
         .await
@@ -17,7 +36,28 @@ async fn main() {
 
     let app = Router::new()
         .with_state(postgres_pool)
-        .merge(routes::router());
+        .merge(routes::router())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+        );
+
+    tracing::debug!("debug");
+    tracing::info!("info");
+    tracing::warn!("warn");
+    tracing::error!("error");
 
     axum::serve(listener, app).await.unwrap();
 }
