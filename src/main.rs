0b5651d9343c13761ab7230d5be5def069d1d6cc
lucasfelber <lucas.felber@inode.at>
2024-09-28 21:46:41 +0200

    database connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+use axum::Router;
+
+mod database;
 mod routes;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
+    let postgres_pool = database::postgres::connect()
+        .await
+        .expect("Postgres connection failed");
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
 
-    axum::serve(listener, routes::router()).await.unwrap();
+    let app = Router::new()
+        .with_state(postgres_pool)
+        .merge(routes::router());
+
+    axum::serve(listener, app).await.unwrap();
 }
